3c1b72f32329394a4539da123b4716bf393b8c25
Ryan DeFever <rdefeve@g.clemson.edu>
2018-11-29 12:10:50 -0500

    Adding infer_nolabel script

---
+++ scripts/run_infer_nolabel.py
@@ -0,0 +1,52 @@
+
+
+import numpy as np
+import os
+import sys
+import argparse
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, '../models'))
+
+from models.pointnet import PointNet
+
+if __name__ == '__main__':
+
+	#Parse Arguments
+    parser = argparse.ArgumentParser(description='Run inference on specified dataset')
+    parser.add_argument('--weights', help='folder containing network weights to use', type=str, required=True)
+    parser.add_argument('--dataset', help='dataset to be used (numpy format)', type=str, required=True)
+
+    args = parser.parse_args()
+
+    # Load data and labels -- 
+    d = np.load(args.dataset)
+    # Extract just coords
+    dataset = d[:,:,2:]
+    print("Dataset shape:")
+    print(dataset.shape)
+    nsamples = dataset.shape[0]
+    npoints = dataset.shape[1]
+    # Extract just atom/frids
+    ids = d[:,0,:2]
+
+    pc = PointNet(n_points=npoints, weights_file=args.weights)
+
+    result = pc.infer_nolabel(dataset)
+    np_result = np.asarray(result)
+    nsamples = np_result.shape[0]*np_result.shape[1]
+    np_result = np_result.reshape((nsamples,1))
+    print("Result shape:")
+    print(np_result.shape)
+
+    ids = ids[:nsamples]
+
+    final = np.hstack((ids,np_result))
+
+    f = open("classification.out",'w')
+    for i in range(len(final)):
+        f.write("{:10d}\t{:10d}\t{:4d}\n".format(int(final[i][0]),int(final[i][1]),int(final[i][2])))
+    
+    f.close()
+
