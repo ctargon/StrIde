b74770fe5807f9fb1b2a4f34cb97d98589f48b22
Colin Targonski <ctargon@g.clemson.edu>
2019-01-14 19:33:19 -0500

    add vae support

--- scripts/run_ae_pointnet.py
+++ scripts/run_ae_pointnet.py
@@ -19,6 +19,7 @@ sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, '../'))
 
 from models.ae_pointnet import PointNet_AE
+from models.vae_pointnet import PointNet_VAE
 from utils.DataContainer import DataContainer as DC
 
 
@@ -30,6 +31,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Run classification on specified dataset')
 	parser.add_argument('--dataset', help='dataset to be used (numpy format)', type=str, required=True)
 	parser.add_argument('--labels', help='labels corresponding to dataset (numpy format)', type=str, required=True)
+	parser.add_argument('--model', help='ae/vae vanilla autoencoder or variational autoencoder', type=str, required=True, choices=['ae', 'vae'])
 	parser.add_argument('--weights', help='folder to save weights to', type=str, required=True)
 
 
@@ -46,15 +48,25 @@ if __name__ == '__main__':
 	print('converting to DataContainer format...')
 	dc = DC(data=data, labels=labels)
 
-	net = PointNet_AE(epochs=40,
-				   batch_size=64,
-				   n_points=dc.train.data.shape[1],
-				   n_classes=dc.train.labels.shape[-1],
-				   n_input=3, 
-				   verbose=1,
-				   save=1,
-                   weights_dir=args.weights)
-
+	if args.model == 'ae':
+		net = PointNet_AE(epochs=40,
+					   batch_size=64,
+					   n_points=dc.train.data.shape[1],
+					   n_classes=dc.train.labels.shape[-1],
+					   n_input=3, 
+					   verbose=1,
+					   save=1,
+	                   weights_dir=args.weights)
+	elif args.model == 'vae':
+		net = PointNet_AE(epochs=40,
+					   batch_size=64,
+					   latent_dim=64,
+					   n_points=dc.train.data.shape[1],
+					   n_classes=dc.train.labels.shape[-1],
+					   n_input=3, 
+					   verbose=1,
+					   save=1,
+	                   weights_dir=args.weights)
 
 	print('train shape: ' + str(dc.train.data.shape))
 	print('label shape: ' + str(dc.train.labels.shape))
