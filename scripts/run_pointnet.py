a8273579d50bf0bcbac814c72244a60aba3f7b80
Ryan DeFever <rdefeve@g.clemson.edu>
2019-02-01 12:41:32 -0500

    Adding nepochs flag and making pc default network

--- scripts/run_pointnet.py
+++ scripts/run_pointnet.py
@@ -23,9 +23,6 @@ from models.cnn import CNN
 from models.pointnet import PointNet
 from utils.DataContainer import DataContainer as DC
 
-
-
-
 if __name__ == '__main__':
 
 	#Parse Arguments
@@ -33,8 +30,9 @@ if __name__ == '__main__':
 	parser.add_argument('--dataset', help='dataset to be used (numpy format)', type=str, required=True)
 	parser.add_argument('--labels', help='labels corresponding to dataset (numpy format)', type=str, required=True)
 	parser.add_argument('--net', help='which type of network to run (mlp/cnn)', type=str, required=False, \
-									choices=['mlp', 'cnn', 'pc'], default='mlp')
+									choices=['mlp', 'cnn', 'pc'], default='pc')
 	parser.add_argument('--weights', help='folder to save weights to', type=str, required=True)
+	parser.add_argument('--nepochs', help='number of epochs for training', type=int, required=False,default=100)
 	parser.add_argument('--noise', help='type of noise to input', type=str, required=True, default='normal')
 	parser.add_argument('--p1', help='param 1 for noise (mu if normal, lower bound if uniform)', type=float, required=True)
 	parser.add_argument('--p2', help='param 2 for noise (sigma if normal, upper bound if uniform)', type=float, required=True)
@@ -48,32 +46,26 @@ if __name__ == '__main__':
 
 	print('loading numpy data...')
 	data = np.load(args.dataset)
-	#data = data[:,:,2:]
 	labels = np.load(args.labels)
 
 	print('converting to DataContainer format...')
 	dc = DC(data=data, labels=labels)
 
 	if args.net == 'mlp':
-		# dc.train.data = dc.train.data.reshape(dc.train.data.shape[0], -1)
-		# dc.test.data = dc.test.data.reshape(dc.test.data.shape[0], -1)
-		# triu_i = np.triu_indices(dc.train.data.shape[-1], k=1)
-		# dc.train.data = dc.train.data[:, triu_i[0], triu_i[1]]
-		# dc.test.data = dc.test.data[:, triu_i[0], triu_i[1]]
-		net = MLP(epochs=40,
+		net = MLP(epochs=args.nepochs,
 				  h_units=[512, 128, 32],
 				  batch_size=64,
 				  n_input=dc.train.data.shape[-1],
 				  verbose=1)
 	
 	if args.net == 'cnn':
-		net = CNN(epochs=20,
+		net = CNN(epochs=args.nepochs,
 				  batch_size=64,
 				  n_input=dc.train.data.shape[-1],
 				  verbose=1)
 
 	if args.net == 'pc':
-		net = PointNet(epochs=100,
+		net = PointNet(epochs=args.nepochs,
 					   batch_size=64,
 					   n_points=dc.train.data.shape[1],
 					   n_classes=dc.train.labels.shape[-1],
